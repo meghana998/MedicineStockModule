c97b35b163ac5bba888f2b70ca2d00e299ca2cac
matta.meghana@gmail.com <matta.meghana@gmail.com>
2020-12-15 11:00:03 +0530

    added unit tests and updated back to old data

--- MedicineStockModule/Repository/MedicineStockRepository.cs
+++ MedicineStockModule/Repository/MedicineStockRepository.cs
@@ -11,31 +11,46 @@ namespace MedicineStockModule.Repository
         public static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         private static readonly List<MedicineStockDTO> MedicineStockInformation = new List<MedicineStockDTO>() {
-            new MedicineStockDTO{Name="Aspirin",
-                              ChemicalComposition=new List<string>{"Acetylsalicyclic acid"},
-                              TargetAilment="General",
-                              DateOfExpiry=DateTime.Parse("22-10-2021"),
-                              NumberOfTabletsInStock=250},
-            new MedicineStockDTO{Name="Codeine",
-                              ChemicalComposition=new List<string>{"serotonin"},
-                              TargetAilment="Orthopaedics",
-                              DateOfExpiry=DateTime.Parse("10-09-2021"),
-                              NumberOfTabletsInStock=150},
-            new MedicineStockDTO{Name="Mifepristone",
-                              ChemicalComposition=new List<string>{"methotrexate"},
-                              TargetAilment="Gynecology",
-                              DateOfExpiry=DateTime.Parse("10-10-2021"),
-                              NumberOfTabletsInStock=100},
-            new MedicineStockDTO{Name="Combiflam",
-                              ChemicalComposition=new List<string>{"Acetaminophen"},
-                              TargetAilment="Orthopaedics",
-                              DateOfExpiry=DateTime.Parse("10-01-2021"),
-                              NumberOfTabletsInStock=50},
-            new MedicineStockDTO{Name="Misoprostol",
-                              ChemicalComposition=new List<string>{"Adenylate cyclase"},
-                              TargetAilment="Gynecology",
-                              DateOfExpiry=DateTime.Parse("10-10-2021"),
-                              NumberOfTabletsInStock=50},
+           new MedicineStockDTO
+            {
+                Name = "Medicine1",
+                ChemicalComposition = new List<string> { "chemical1", "chemical2" },
+                TargetAilment = "Orthopaedics",
+                DateOfExpiry = DateTime.Parse("10-10-2021"),
+                NumberOfTabletsInStock = 50
+            },
+            new MedicineStockDTO
+            {
+                Name = "Medicine2",
+                ChemicalComposition = new List<string> { "chemical3", "chemical2" },
+                TargetAilment = "General",
+                DateOfExpiry = DateTime.Parse("10-09-2021"),
+                NumberOfTabletsInStock = 50
+            },
+            new MedicineStockDTO
+            {
+                Name = "Medicine3",
+                ChemicalComposition = new List<string> { "chemical1", "chemical2" },
+                TargetAilment = "Gynecology",
+                DateOfExpiry = DateTime.Parse("10-10-2021"),
+                NumberOfTabletsInStock = 50
+            },
+            new MedicineStockDTO
+            {
+                Name = "Medicine4",
+                ChemicalComposition = new List<string> { "chemical1", "chemical2" },
+                TargetAilment = "Gynecology",
+                DateOfExpiry = DateTime.Parse("10-10-2021"),
+                NumberOfTabletsInStock = 50
+            },
+            new MedicineStockDTO
+            {
+                Name = "Medicine5",
+                ChemicalComposition = new List<string> { "chemical1", "chemical2" },
+                TargetAilment = "General",
+                DateOfExpiry = DateTime.Parse("10-10-2021"),
+                NumberOfTabletsInStock = 50
+            },
 
         };
         
